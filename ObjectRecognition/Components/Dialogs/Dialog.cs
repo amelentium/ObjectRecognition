eec7dab03e0b63f0a43a23199416df0295249198
Andriy Meleniuk <amelentium@gmail.com>
2023-11-20 16:25:06 +0200

    Refactoring dialog components

---
+++ ObjectRecognition/Components/Dialogs/Dialog.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ObjectRecognition.Components.Dialogs
+{
+    public class DialogBase : ComponentBase
+    {
+        protected bool IsOpen = false;
+
+        protected void ShowDialog()
+        {
+            IsOpen = true;
+        }
+
+        protected void CloseDialog()
+        {
+            IsOpen = false;
+        }
+
+        public void Show()
+        {
+            ShowDialog();
+        }
+    }
+
+    public class Dialog : DialogBase
+    {
+        [Parameter]
+        public EventCallback OnConfirm { get; set; }
+
+        protected void ConfirmAction(object arg = null)
+        {
+            OnConfirm.InvokeAsync(arg);
+            IsOpen = false;
+        }
+    }
+
+    public class Dialog<T> : DialogBase
+    {
+        [Parameter]
+        public EventCallback<T> OnConfirm { get; set; }
+
+        protected void ConfirmAction(T arg)
+        {
+            OnConfirm.InvokeAsync(arg);
+            IsOpen = false;
+        }
+    }
+}
